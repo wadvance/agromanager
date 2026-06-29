6aec0858adee31f2e514c2075f2d58aa822721c5
wadvance <barj23535@gmail.com>
2026-06-28 22:49:53 -0500

    fix: add firebase_options.dart for web Firebase initialization

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import '../firebase_options.dart';
 import '../models/crop.dart';
 import '../models/livestock.dart';
 import '../models/inventory_item.dart';
@@ -12,7 +13,9 @@ class FirebaseService {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   static Future<void> initialize() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 
   static User? get currentUser => _auth.currentUser;
